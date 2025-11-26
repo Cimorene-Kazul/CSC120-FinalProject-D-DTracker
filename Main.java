af29034aa907ad59fe98bae1e9b3ab527feb8d7d
Cimorene-Kazul <eliana.lippa@gmail.com>
2025-11-26 11:12:36 -0500

    modified to run the builder

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Player moth = new Player("Moth", "Elizabeth");
-        Player emia = new Player("Emia", "Celina");
-        Monster dragon1 = new Monster("ancient_green_dragon");
-        Monster dragon2 = new Monster("ancient_green_dragon");
-        Creature[] creatures = {moth, emia, dragon1, dragon2};
-        InitiativeTracker myEncounter = new InitiativeTracker(creatures);
-        myEncounter.rollInitiative();
+        EncounterBuilder myEncounterBuilder = new EncounterBuilder();
+        myEncounterBuilder.buildEncounter();
     }
 }
